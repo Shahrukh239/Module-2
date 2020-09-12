3751a0ae30c82ea438f6ecabf141b4a56ed1bfac
Shahrukh Raza <66723685+Shahrukh239@users.noreply.github.com>
2020-09-12 17:07:15 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+	static void Main(string[] args) // Example of Nested-if-else Statement
+	{
+		string name,length;
+		Console.WriteLine("Enter your username: ");
+		name = Console.ReadLine();
+		Console.WriteLine("enter length");
+		length = Console.ReadLine();
+
+		if (name == "Mahbub" && length =="6")
+		{
+			Console.WriteLine("Hi Mahbub!");
+		}
+		else if  (name.Length == 0)
+		{
+			Console.WriteLine("No username was entered.");
+		}
+		else
+		{
+			Console.WriteLine("The user {0},{1} has not been registered.",name,length);
+		}
+		Console.Read();
+	}
+}
